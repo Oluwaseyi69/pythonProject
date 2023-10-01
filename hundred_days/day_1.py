87f1f11b33bd931908846345bf73380a1bb50636
Oluwaseyi69 <oluwaseyitemitope69@gmail.com>
2023-10-01 16:01:10 +0100

    updated files

---
+++ hundred_days/day_1.py
@@ -0,0 +1,11 @@
+# print('Hello world!!\nHello world!!')
+# print("Hello" + " " +"Seyi")
+
+# print("Day 1-String manipulation")
+# print("String concatenation is done with the + sign.")
+# print("e.g. print('Hello' + 'world')")
+# print("New lines can be created with a backslash and n ")
+
+# print("Hello " + input("what is your name: "))
+name = input("Enter your name: ")
+print(len(name))
